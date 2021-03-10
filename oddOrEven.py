cac92f5526f3c09b4ffb0f58dd05cad51714832e
Squidjigg <squidjigg80@outlook.com>
2021-03-10 22:09:13 +1100

    add comments

--- oddOrEven.py
+++ oddOrEven.py
@@ -1,15 +1,16 @@
 userInput = ''
-#result = userNumber % 2
-#print(result)
+# contine until user enters q to quit.
 while userInput != 'q':
 
     userInput = input("Type in a number between 1 and 1000 (type q to quit): ")
     if userInput == 'q':
         break
     
+    # convert input var to int.
     userNumber = int(userInput)
     print(userNumber)
 
+    # if remainder of division equals 0, then output that it is even, otherwise output that it is odd.
     if((userNumber % 2) == 0):
         print("That is an even number. Try again?")
     else:
