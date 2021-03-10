2f15a4a076961f05527a6ed9dee9f13b4d5c156a
Squidjigg <squidjigg80@outlook.com>
2021-03-10 22:48:03 +1100

    Initial committ

---
+++ madLibs.py
@@ -0,0 +1,11 @@
+# define var for inputs.
+colour = input("Enter a colour: ")
+pluralNoun = input("Enter a plural noun: ")
+item = input("Enter an item: ")
+celebrity = input("Enter a celebrity name: " )
+
+# print out results.
+print(f"\nRoses are {colour},")
+print(f"{pluralNoun.capitalize()} are blue.")
+print(f"{item.capitalize()} is sweet,")
+print(f"And I love {celebrity.title()}.")
